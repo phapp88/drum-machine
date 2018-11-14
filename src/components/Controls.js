0b2931632b5e8c8584b4d86d780d054f95e2554b
Paul <phapp1988@gmail.com>
2018-11-14 09:59:24 -0600

    Address security vulnerability

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,6 +4,7 @@ const Controls = ({ clip }) => (
   <div id="controls">
     {/* TODO: power button, volume meter, dropdown for other sets of clips*/}
     <p id="display">{clip}</p>
+    <input type="range" />
   </div>
 );
 
